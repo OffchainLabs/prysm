3ed697fee0aeaba435baae1e0928b804f1758e7d
jsvisa <delweng@gmail.com>
2024-06-02 00:54:32 +0000

    config/params: split testutils into normal,prod,dev
    Signed-off-by: jsvisa <delweng@gmail.com>

--- config/params/testutils.go
+++ config/params/testutils.go
@@ -1,5 +1,3 @@
-//go:build !develop
-
 package params
 
 import "testing"
@@ -8,22 +6,17 @@ const (
 	EnvNameOverrideAccept = "PRYSM_API_OVERRIDE_ACCEPT"
 )
 
-// SetupTestConfigCleanup preserves configurations allowing to modify them within tests without any
-// restrictions, everything is restored after the test.
-func SetupTestConfigCleanup(t testing.TB) {
-	prevDefaultBeaconConfig := mainnetBeaconConfig.Copy()
-	temp := configs.getActive().Copy()
-	undo, err := SetActiveWithUndo(temp)
+// SetActiveTestCleanup sets an active config,
+// and adds a test cleanup hook to revert to the default config after the test completes.
+func SetActiveTestCleanup(t *testing.T, cfg *BeaconChainConfig) {
+	undo, err := SetActiveWithUndo(cfg)
 	if err != nil {
 		t.Fatal(err)
 	}
-	prevNetworkCfg := networkConfig.Copy()
 	t.Cleanup(func() {
-		mainnetBeaconConfig = prevDefaultBeaconConfig
 		err = undo()
 		if err != nil {
 			t.Fatal(err)
 		}
-		networkConfig = prevNetworkCfg
 	})
 }
