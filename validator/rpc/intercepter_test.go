c8ae5c5506073d85f6819e6783398c567a17b1bc
james-prysm <90280386+james-prysm@users.noreply.github.com>
2024-10-29 09:04:51 -0500

    Update validator/rpc/intercepter_test.go
    Co-authored-by: Sammy Rosso <15244892+saolyn@users.noreply.github.com>

--- validator/rpc/intercepter_test.go
+++ validator/rpc/intercepter_test.go
@@ -76,7 +76,7 @@ func TestServer_AuthTokenHandler(t *testing.T) {
 		require.Equal(t, http.StatusUnauthorized, rr.Code)
 		errJson := &httputil.DefaultJsonError{}
 		require.NoError(t, json.Unmarshal(rr.Body.Bytes(), errJson))
-		require.StringContains(t, "unauthorized", errJson.Message)
+		require.StringContains(t, "Unauthorized", errJson.Message)
 	})
 	t.Run("wrong auth token was sent", func(t *testing.T) {
 		rr := httptest.NewRecorder()
