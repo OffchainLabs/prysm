f37f0c215b44f5da0918d96e5362006b7df4e4d5
terence tsao <terence@prysmaticlabs.com>
2024-12-16 17:23:14 -0800

    Handle proposer computations in between forks

--- config/fieldparams/minimal.go
+++ config/fieldparams/minimal.go
@@ -27,7 +27,9 @@ const (
 	SyncAggregateSyncCommitteeBytesLength = 4                 // SyncAggregateSyncCommitteeBytesLength defines the length of sync committee bytes in a sync aggregate.
 	MaxWithdrawalsPerPayload              = 4                 // MaxWithdrawalsPerPayloadLength defines the maximum number of withdrawals that can be included in a payload.
 	MaxBlobCommitmentsPerBlock            = 16                // MaxBlobCommitmentsPerBlock defines the theoretical limit of blobs can be included in a block.
+	MaxBlobCommitmentsPerBlockElectra     = 32                // MaxBlobCommitmentsPerBlockElectra defines the theoretical limit of blobs can be included in a block. (This only applies to minimal config post Electra)
 	LogMaxBlobCommitments                 = 4                 // Log_2 of MaxBlobCommitmentsPerBlock
+	LogMaxBlobCommitmentsElectra          = 5                 // Log_2 of MaxBlobCommitmentsPerBlockElectra. (This only applies to minimal config post Electra)
 	BlobLength                            = 131072            // BlobLength defines the byte length of a blob.
 	BlobSize                              = 131072            // defined to match blob.size in bazel ssz codegen
 	BlobSidecarSize                       = 131928            // defined to match blob sidecar size in bazel ssz codegen
@@ -51,5 +53,6 @@ const (
 	MaxDeposits                           = 16                // Maximum number of deposits in a block.
 	MaxVoluntaryExits                     = 16                // Maximum number of voluntary exits in a block.
 	MaxBlsToExecutionChanges              = 16                // Maximum number of bls to execution changes in a block.
-	MaxRandomValue                        = uint64(1<<16 - 1) // Maximum value for a random value using for proposer and sync committee sampling.
+	MaxRandomValue                        = uint64(1<<8 - 1)  // Maximum value for a random value using for proposer and sync committee sampling.
+	MaxRandomValueElectra                 = uint64(1<<16 - 1) // Maximum value for a random value using for proposer and sync committee sampling.
 )
