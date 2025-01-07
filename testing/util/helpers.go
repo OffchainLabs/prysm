c3597dfaa80efad752e58d614f8c5ec055b940e7
Manu NALEPA <enalepa@offchainlabs.com>
2025-01-07 15:59:52 +0100

    Update testing/util/helpers.go
    Co-authored-by: Radosław Kapka <rkapka@wp.pl>

--- testing/util/helpers.go
+++ testing/util/helpers.go
@@ -155,7 +155,7 @@ func Random32Bytes(t *testing.T) []byte {
 
 // HackForksMaxuint is helpful for tests that need to set up cases for some future forks.
 // We have unit tests that assert our config matches the upstream config, where some forks epoch are always
-// set to MaxUint64 until they aer formally set. This creates an issue for tests that want to
+// set to MaxUint64 until they are formally set. This creates an issue for tests that want to
 // work with slots that are defined to be after these forks because converting the max epoch to a slot leads
 // to multiplication overflow.
 // Monkey patching tests with this function is the simplest workaround in these cases.
