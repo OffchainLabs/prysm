8a997e7c66b93f3a7b843ace677e9f214df38fe1
james-prysm <james@prysmaticlabs.com>
2025-01-29 16:53:37 -0600

    adding in special condition to pass electra epoch participation

--- testing/endtoend/evaluators/validator.go
+++ testing/endtoend/evaluators/validator.go
@@ -126,6 +126,11 @@ func validatorsParticipating(_ *types.EvaluationContext, conns ...*grpc.ClientCo
 	if e2eparams.TestParams.LighthouseBeaconNodeCount != 0 {
 		expected = float32(expectedMulticlientParticipation)
 	}
+	if participation.Epoch == params.BeaconConfig().ElectraForkEpoch {
+		// The first slot of Electra will be missed due to the switching of attestation types
+		// 5/6 slots =~0.83
+		expected = 0.83
+	}
 	if participation.Epoch > 0 && participation.Epoch.Sub(1) == params.BeaconConfig().BellatrixForkEpoch {
 		// Reduce Participation requirement to 95% to account for longer EE calls for
 		// the merge block. Target and head will likely be missed for a few validators at
