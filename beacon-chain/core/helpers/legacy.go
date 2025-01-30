e165797bbe28e9d9f81947d88f19d9526fcb7577
james-prysm <90280386+james-prysm@users.noreply.github.com>
2025-01-30 09:28:07 -0600

    Update beacon-chain/core/helpers/legacy.go
    Co-authored-by: Jun Song <87601811+syjn99@users.noreply.github.com>

--- beacon-chain/core/helpers/legacy.go
+++ beacon-chain/core/helpers/legacy.go
@@ -5,17 +5,16 @@ import (
 	"github.com/prysmaticlabs/prysm/v5/runtime/version"
 )
 
-// DepositRequestsHaveStarted determines if the deposit requests have started
-func DepositRequestsHaveStarted(beaconState state.BeaconState) bool {
-	if beaconState.Version() >= version.Electra {
-		requestsStartIndex, err := beaconState.DepositRequestsStartIndex()
-		if err == nil {
-			// deposit_requests_start_index will only be set once
-			// eth1data will be frozen
-			if beaconState.Eth1DepositIndex() == requestsStartIndex {
-				return true
-			}
-		}
-	}
-	return false
+// HasDepositRequestStarted determines if the deposit requests have started.
+func HasDepositRequestStarted(beaconState state.BeaconState) bool {
+    if beaconState.Version() < version.Electra {
+        return false
+    }
+
+    requestsStartIndex, err := beaconState.DepositRequestsStartIndex()
+    if err != nil {
+        return false
+    }
+
+    return beaconState.Eth1DepositIndex() == requestsStartIndex
 }
