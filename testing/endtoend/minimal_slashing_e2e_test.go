8975a7a66562e1e1be327e1961eb9c334c1a8bac
nisdas <nishdas93@gmail.com>
2025-02-20 16:39:22 +0800

    Fix Slashing Config

--- testing/endtoend/minimal_slashing_e2e_test.go
+++ testing/endtoend/minimal_slashing_e2e_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/prysmaticlabs/prysm/v5/config/params"
+	"github.com/prysmaticlabs/prysm/v5/runtime/version"
 	ev "github.com/prysmaticlabs/prysm/v5/testing/endtoend/evaluators"
 	e2eParams "github.com/prysmaticlabs/prysm/v5/testing/endtoend/params"
 	"github.com/prysmaticlabs/prysm/v5/testing/endtoend/types"
@@ -13,7 +14,7 @@ import (
 
 func TestEndToEnd_Slasher_MinimalConfig(t *testing.T) {
 	params.SetupTestConfigCleanup(t)
-	params.OverrideBeaconConfig(params.E2ETestConfig().Copy())
+	params.OverrideBeaconConfig(types.InitForkCfg(version.Bellatrix, version.Electra, params.E2ETestConfig()))
 	require.NoError(t, e2eParams.Init(t, e2eParams.StandardBeaconCount))
 
 	tracingPort := e2eParams.TestParams.Ports.JaegerTracingPort
