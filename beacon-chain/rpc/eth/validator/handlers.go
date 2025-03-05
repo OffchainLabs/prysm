fff8f7276fbf19121016ab01de0907266e9f58bc
james-prysm <james@prysmaticlabs.com>
2025-03-05 13:48:39 -0600

    fixing error message

--- beacon-chain/rpc/eth/validator/handlers.go
+++ beacon-chain/rpc/eth/validator/handlers.go
@@ -638,11 +638,12 @@ func (s *Server) ProduceSyncCommitteeContribution(w http.ResponseWriter, r *http
 
 	isOptimistic, err := s.OptimisticModeFetcher.IsOptimistic(ctx)
 	if err != nil {
-		httputil.HandleError(w, "Beacon node is currently syncing and not serving request on that endpoint", http.StatusServiceUnavailable)
+		httputil.HandleError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	if isOptimistic {
-		httputil.HandleError(w, "Optimistic mode fetcher is optimistic only", http.StatusBadRequest)
+		httputil.HandleError(w, "Beacon node is currently syncing and not serving request on that endpoint", http.StatusServiceUnavailable)
+		return
 	}
 	_, index, ok := shared.UintFromQuery(w, r, "subcommittee_index", true)
 	if !ok {
