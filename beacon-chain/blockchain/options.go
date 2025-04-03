f75439c4659aa78155e1274fc027f66fa718a6a7
Bastin <bastin.m@proton.me>
2025-04-03 17:26:47 +0200

    add lcstore to the blockchain package

--- beacon-chain/blockchain/options.go
+++ beacon-chain/blockchain/options.go
@@ -4,6 +4,7 @@ import (
 	"github.com/prysmaticlabs/prysm/v5/async/event"
 	"github.com/prysmaticlabs/prysm/v5/beacon-chain/cache"
 	statefeed "github.com/prysmaticlabs/prysm/v5/beacon-chain/core/feed/state"
+	lightclient "github.com/prysmaticlabs/prysm/v5/beacon-chain/core/light-client"
 	"github.com/prysmaticlabs/prysm/v5/beacon-chain/db"
 	"github.com/prysmaticlabs/prysm/v5/beacon-chain/db/filesystem"
 	"github.com/prysmaticlabs/prysm/v5/beacon-chain/execution"
@@ -217,6 +218,13 @@ func WithSyncChecker(checker Checker) Option {
 func WithSlasherEnabled(enabled bool) Option {
 	return func(s *Service) error {
 		s.slasherEnabled = enabled
+		return nil
+	}
+}
+
+func WithLightClientStore(lcs *lightclient.Store) Option {
+	return func(s *Service) error {
+		s.lcStore = lcs
 		return nil
 	}
 }
