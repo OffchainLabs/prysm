b700ff2ca414ce3bf7a225e546e5c31504087c95
james-prysm <james@prysmaticlabs.com>
2025-04-30 20:13:33 -0500

    gofmt

--- validator/client/beacon-api/propose_beacon_block.go
+++ validator/client/beacon-api/propose_beacon_block.go
@@ -6,10 +6,10 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/pkg/errors"
 	"github.com/OffchainLabs/prysm/v6/api/server/structs"
 	"github.com/OffchainLabs/prysm/v6/network/httputil"
 	ethpb "github.com/OffchainLabs/prysm/v6/proto/prysm/v1alpha1"
+	"github.com/pkg/errors"
 )
 
 type blockProcessingResult struct {
