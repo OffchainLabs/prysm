f98b83c806e02e010fd53286dd0cdd2afb65a3fa
Bastin <bastin.m@proton.me>
2025-06-25 12:57:02 +0200

    bucket | configs | flag | cache

--- beacon-chain/db/kv/kv.go
+++ beacon-chain/db/kv/kv.go
@@ -91,6 +91,7 @@ type Store struct {
 	blockCache          *ristretto.Cache[string, interfaces.ReadOnlySignedBeaconBlock]
 	validatorEntryCache *ristretto.Cache[[]byte, *ethpb.Validator]
 	stateSummaryCache   *stateSummaryCache
+	stateDiffCache      *stateDiffCache
 	ctx                 context.Context
 }
 
@@ -112,6 +113,7 @@ var Buckets = [][]byte{
 	lightClientUpdatesBucket,
 	lightClientBootstrapBucket,
 	lightClientSyncCommitteeBucket,
+	stateDiffBucket,
 	// Indices buckets.
 	blockSlotIndicesBucket,
 	stateSlotIndicesBucket,
@@ -182,6 +184,7 @@ func NewKVStore(ctx context.Context, dirPath string, opts ...KVStoreOption) (*St
 		blockCache:          blockCache,
 		validatorEntryCache: validatorCache,
 		stateSummaryCache:   newStateSummaryCache(),
+		stateDiffCache:      newStateDiffCache(),
 		ctx:                 ctx,
 	}
 	for _, o := range opts {
---
+++ config/params/state_diff_config.go
@@ -0,0 +1,9 @@
+package params
+
+var (
+	stateHierarchyExponents = []uint64{21, 18, 16, 13, 11, 9, 5}
+)
+
+func StateHierarchyExponents() []uint64 {
+	return stateHierarchyExponents
+}
