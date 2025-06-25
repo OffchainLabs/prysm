8a231c57beaf91264572c32da17014e1d21517c1
Bastin <bastin.m@proton.me>
2025-06-25 21:01:24 +0200

    remove named mutex

--- beacon-chain/db/kv/state_diff_cache.go
+++ beacon-chain/db/kv/state_diff_cache.go
@@ -12,7 +12,7 @@ import (
 const unset = math.MaxUint64
 
 type stateDiffCache struct {
-	mu      sync.RWMutex
+	sync.RWMutex
 	anchors []state.ReadOnlyBeaconState
 	offset  uint64
 }
@@ -25,20 +25,20 @@ func newStateDiffCache() *stateDiffCache {
 }
 
 func (c *stateDiffCache) getAnchor(level int) state.ReadOnlyBeaconState {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
+	c.RLock()
+	defer c.RUnlock()
 	return c.anchors[level]
 }
 
 func (c *stateDiffCache) setAnchor(level int, anchor state.ReadOnlyBeaconState) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	c.Lock()
+	defer c.Unlock()
 	c.anchors[level] = anchor
 }
 
 func (c *stateDiffCache) getOffset() (uint64, error) {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
+	c.RLock()
+	defer c.RUnlock()
 	if c.offset == unset {
 		return 0, errors.New("offset is not set")
 	}
@@ -46,13 +46,13 @@ func (c *stateDiffCache) getOffset() (uint64, error) {
 }
 
 func (c *stateDiffCache) setOffset(offset uint64) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	c.Lock()
+	defer c.Unlock()
 	c.offset = offset
 }
 
 func (c *stateDiffCache) clearAnchors() {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	c.Lock()
+	defer c.Unlock()
 	c.anchors = make([]state.ReadOnlyBeaconState, len(params.StateHierarchyExponents()))
 }
