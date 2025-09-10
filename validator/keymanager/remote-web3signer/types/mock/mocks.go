a147038e1251b87e16d64aecb7b7acf78d23fbf5
james-prysm <james@prysmaticlabs.com>
2025-09-10 11:12:03 -0500

    fixing test as v1 proof is no longer used

--- validator/keymanager/remote-web3signer/types/mock/mocks.go
+++ validator/keymanager/remote-web3signer/types/mock/mocks.go
@@ -53,32 +53,6 @@ func GetMockSignRequest(t string) *validatorpb.SignRequest {
 			},
 			SigningSlot: 0,
 		}
-	case "AGGREGATE_AND_PROOF":
-		return &validatorpb.SignRequest{
-			PublicKey:       make([]byte, fieldparams.BLSPubkeyLength),
-			SigningRoot:     make([]byte, fieldparams.RootLength),
-			SignatureDomain: make([]byte, 4),
-			Object: &validatorpb.SignRequest_AggregateAttestationAndProof{
-				AggregateAttestationAndProof: &eth.AggregateAttestationAndProof{
-					AggregatorIndex: 0,
-					Aggregate: &eth.Attestation{
-						AggregationBits: bitfield.Bitlist{0b1101},
-						Data: &eth.AttestationData{
-							BeaconBlockRoot: make([]byte, fieldparams.RootLength),
-							Source: &eth.Checkpoint{
-								Root: make([]byte, fieldparams.RootLength),
-							},
-							Target: &eth.Checkpoint{
-								Root: make([]byte, fieldparams.RootLength),
-							},
-						},
-						Signature: make([]byte, 96),
-					},
-					SelectionProof: make([]byte, fieldparams.BLSSignatureLength),
-				},
-			},
-			SigningSlot: 0,
-		}
 	case "AGGREGATE_AND_PROOF_V2":
 		return &validatorpb.SignRequest{
 			PublicKey:       make([]byte, fieldparams.BLSPubkeyLength),
