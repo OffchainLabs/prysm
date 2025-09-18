8f9026bed8839646d0eb2fa91caff6afcf28d4e6
Manu NALEPA <enalepa@offchainlabs.com>
2025-09-18 18:53:27 +0200

    Fix deadlock-option 2.

--- beacon-chain/rpc/prysm/v1alpha1/validator/proposer.go
+++ beacon-chain/rpc/prysm/v1alpha1/validator/proposer.go
@@ -433,13 +433,25 @@ func (vs *Server) handleUnblindedBlock(
 
 // broadcastReceiveBlock broadcasts a block and handles its reception.
 func (vs *Server) broadcastReceiveBlock(ctx context.Context, wg *sync.WaitGroup, block interfaces.SignedBeaconBlock, root [fieldparams.RootLength]byte) error {
+	if err := vs.broadcastBlock(ctx, wg, block, root); err != nil {
+		return errors.Wrap(err, "broadcast block")
+	}
+
+	if err := vs.BlockReceiver.ReceiveBlock(ctx, block, root, nil); err != nil {
+		return errors.Wrap(err, "receive block")
+	}
+
+	return nil
+}
+
+func (vs *Server) broadcastBlock(ctx context.Context, wg *sync.WaitGroup, block interfaces.SignedBeaconBlock, root [fieldparams.RootLength]byte) error {
+	defer wg.Done()
+
 	protoBlock, err := block.Proto()
 	if err != nil {
-		wg.Done()
 		return errors.Wrap(err, "protobuf conversion failed")
 	}
 	if err := vs.P2P.Broadcast(ctx, protoBlock); err != nil {
-		wg.Done()
 		return errors.Wrap(err, "broadcast failed")
 	}
 	vs.BlockNotifier.BlockFeed().Send(&feed.Event{
@@ -452,8 +464,7 @@ func (vs *Server) broadcastReceiveBlock(ctx context.Context, wg *sync.WaitGroup,
 		"root": fmt.Sprintf("%#x", root),
 	}).Debug("Broadcasted block")
 
-	wg.Done()
-	return vs.BlockReceiver.ReceiveBlock(ctx, block, root, nil)
+	return nil
 }
 
 // broadcastAndReceiveBlobs handles the broadcasting and reception of blob sidecars.
