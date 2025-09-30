6e7d4cefa813517c7c9c009cc8d79cd374215602
Manu NALEPA <enalepa@offchainlabs.com>
2025-09-30 12:51:48 +0200

    `logCheckSubscribableError`: Add agent.

--- beacon-chain/p2p/pubsub_filter.go
+++ beacon-chain/p2p/pubsub_filter.go
@@ -130,6 +130,7 @@ func (s *Service) logCheckSubscribableError(pid peer.ID) func(string) bool {
 				log.WithError(err).WithFields(logrus.Fields{
 					"peerID": pid,
 					"topic":  topic,
+					"agent":  agentString(pid, s.Host()),
 				}).Debug("Peer subscription rejected")
 			}
 			return false
