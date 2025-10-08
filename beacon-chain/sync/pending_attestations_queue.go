f31593c796e1c1c5681dcfbcf1b6137ca0375010
james-prysm <james@prysmaticlabs.com>
2025-10-08 11:27:26 -0500

    cleaning up some code

--- beacon-chain/sync/pending_attestations_queue.go
+++ beacon-chain/sync/pending_attestations_queue.go
@@ -241,6 +241,9 @@ func (s *Service) saveAttestation(att ethpb.Att) error {
 	if features.Get().EnableExperimentalAttestationPool {
 		return s.cfg.attestationCache.Add(att)
 	}
+	if att.IsAggregated() {
+		return s.cfg.attPool.SaveAggregatedAttestation(att)
+	}
 	return s.cfg.attPool.SaveUnaggregatedAttestation(att)
 }
 
@@ -300,40 +303,29 @@ func (s *Service) processVerifiedAttestation(
 }
 
 func (s *Service) processAggregate(ctx context.Context, aggregate ethpb.SignedAggregateAttAndProof) {
-	att := aggregate.AggregateAttestationAndProof().AggregateVal()
+	aap := aggregate.AggregateAttestationAndProof()
+	att := aap.AggregateVal()
 
-	// Save the pending aggregated attestation to the pool if it passes the aggregated
-	// validation steps.
-	valRes, err := s.validateAggregatedAtt(ctx, aggregate)
-	if err != nil {
-		log.WithError(err).Debug("Pending aggregated attestation failed validation")
+	if res, err := s.validateAggregatedAtt(ctx, aggregate); err != nil || res != pubsub.ValidationAccept {
+		if err != nil {
+			log.WithError(err).Debug("Pending aggregated attestation failed validation")
+		}
 		return
 	}
-	aggValid := pubsub.ValidationAccept == valRes
-	if aggValid && s.validateBlockInAttestation(ctx, aggregate) {
-		if features.Get().EnableExperimentalAttestationPool {
-			if err = s.cfg.attestationCache.Add(att); err != nil {
-				log.WithError(err).Debug("Could not save aggregated attestation")
-				return
-			}
-		} else {
-			if att.IsAggregated() {
-				if err = s.cfg.attPool.SaveAggregatedAttestation(att); err != nil {
-					log.WithError(err).Debug("Could not save aggregated attestation")
-					return
-				}
-			} else if err = s.cfg.attPool.SaveUnaggregatedAttestation(att); err != nil {
-				log.WithError(err).Debug("Could not save unaggregated attestation")
-				return
-			}
-		}
 
-		s.setAggregatorIndexEpochSeen(att.GetData().Target.Epoch, aggregate.AggregateAttestationAndProof().GetAggregatorIndex())
+	if !s.validateBlockInAttestation(ctx, aggregate) {
+		return
+	}
 
-		// Broadcasting the signed attestation again once a node is able to process it.
-		if err := s.cfg.p2p.Broadcast(ctx, aggregate); err != nil {
-			log.WithError(err).Debug("Could not broadcast")
-		}
+	if err := s.saveAttestation(att); err != nil {
+		log.WithError(err).Debug("Could not save aggregated attestation")
+		return
+	}
+
+	s.setAggregatorIndexEpochSeen(att.GetData().Target.Epoch, aap.GetAggregatorIndex())
+
+	if err := s.cfg.p2p.Broadcast(ctx, aggregate); err != nil {
+		log.WithError(err).Debug("Could not broadcast aggregated attestation")
 	}
 }
 
