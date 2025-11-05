3f7371445b77ec75cf84d31d57b8f13fe01b3dc2
aarshkshah1992 <aarshkshah1992@gmail.com>
2025-11-05 22:23:26 +0400

    fix test in sync

--- testing/endtoend/evaluators/node.go
+++ testing/endtoend/evaluators/node.go
@@ -54,6 +54,7 @@ var AllNodesHaveSameHead = e2etypes.Evaluator{
 }
 
 func healthzCheck(_ *e2etypes.EvaluationContext, conns ...*grpc.ClientConn) error {
+	
 	count := len(conns)
 	for i := 0; i < count; i++ {
 		resp, err := http.Get(fmt.Sprintf("http://localhost:%d/healthz", e2e.TestParams.Ports.PrysmBeaconNodeMetricsPort+i))
