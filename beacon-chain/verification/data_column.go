1d3949051375461f0166e52ec7719561170e9caf
potuz <potuz@prysmaticlabs.com>
2025-11-06 13:41:08 -0300

    Remove extra file

--- beacon-chain/verification/data_column.go
+++ beacon-chain/verification/data_column.go
@@ -69,6 +69,12 @@ var (
 	errBadTopic       = errors.New("topic is not of the one expected")
 )
 
+type LazyHeadStateProvider struct {
+	HeadStateProvider
+}
+
+var _ HeadStateProvider = &LazyHeadStateProvider{}
+
 type (
 	RODataColumnsVerifier struct {
 		*sharedResources
--- beacon-chain/verification/lazy_head_state.go
+++
@@ -1,7 +0,0 @@
-package verification
-
-type LazyHeadStateProvider struct {
-	HeadStateProvider
-}
-
-var _ HeadStateProvider = &LazyHeadStateProvider{}
