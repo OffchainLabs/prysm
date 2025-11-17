a29c6b3be0ddeb187ba318379aa7d41ec8cf2f46
Galoretka <galoretochka@gmail.com>
2025-11-17 13:48:00 +0200

    fix: recompute context digests using provided valRoot

--- beacon-chain/sync/context.go
+++ beacon-chain/sync/context.go
@@ -1,10 +1,13 @@
 package sync
 
 import (
+	"encoding/binary"
 	"io"
 
 	"github.com/OffchainLabs/prysm/v7/beacon-chain/p2p"
 	"github.com/OffchainLabs/prysm/v7/config/params"
+	"github.com/OffchainLabs/prysm/v7/crypto/hash"
+	ethpb "github.com/OffchainLabs/prysm/v7/proto/prysm/v1alpha1"
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/protocol"
 	"github.com/pkg/errors"
@@ -83,11 +86,33 @@ func validateVersion(version string, stream withProtocol) error {
 type ContextByteVersions map[[4]byte]int
 
 // ContextByteVersionsForValRoot computes a mapping between all possible context bytes values
-// and the runtime/version identifier for the corresponding fork.
+// and the runtime/version identifier for the corresponding fork, deriving fork digests using the
+// provided genesis validators root and applying Fulu BPO mixing when applicable.
 func ContextByteVersionsForValRoot(valRoot [32]byte) (ContextByteVersions, error) {
-	m := make(ContextByteVersions)
-	for _, entry := range params.SortedNetworkScheduleEntries() {
-		m[entry.ForkDigest] = entry.VersionEnum
+	entries := params.SortedNetworkScheduleEntries()
+	m := make(ContextByteVersions, len(entries))
+	for _, entry := range entries {
+		fd := &ethpb.ForkData{
+			CurrentVersion:        entry.ForkVersion[:],
+			GenesisValidatorsRoot: valRoot[:],
+		}
+		root, err := fd.HashTreeRoot()
+		if err != nil {
+			return nil, err
+		}
+		var digest [4]byte
+		copy(digest[:], root[:4])
+		if entry.Epoch >= params.BeaconConfig().FuluForkEpoch {
+			hb := make([]byte, 16)
+			binary.LittleEndian.PutUint64(hb[0:8], uint64(entry.BPOEpoch))
+			binary.LittleEndian.PutUint64(hb[8:], entry.MaxBlobsPerBlock)
+			bpoHash := hash.Hash(hb)
+			digest[0] ^= bpoHash[0]
+			digest[1] ^= bpoHash[1]
+			digest[2] ^= bpoHash[2]
+			digest[3] ^= bpoHash[3]
+		}
+		m[digest] = entry.VersionEnum
 	}
 	return m, nil
 }
