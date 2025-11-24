5692778aaf33354ee804658277a93692b550d588
terence tsao <terence@prysmaticlabs.com>
2025-11-24 15:01:12 -0800

    Check exec enabled condition to capella

--- beacon-chain/core/blocks/payload.go
+++ beacon-chain/core/blocks/payload.go
@@ -90,7 +90,7 @@ func IsExecutionEnabled(st state.ReadOnlyBeaconState, body interfaces.ReadOnlyBe
 	if st == nil || body == nil {
 		return false, errors.New("nil state or block body")
 	}
-	if st.Version() >= version.Bellatrix {
+	if st.Version() >= version.Capella {
 		return true, nil
 	}
 	if IsPreBellatrixVersion(st.Version()) {
