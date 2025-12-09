d4510ef6a815a3d6431ab9c054c4b2bdd88fd5e2
Manu NALEPA <enalepa@offchainlabs.com>
2025-12-09 09:51:35 +0100

    `cellsAndProofsFromStructuredComputationTime`: Move summary buckets

--- beacon-chain/core/peerdas/metrics.go
+++ beacon-chain/core/peerdas/metrics.go
@@ -18,7 +18,7 @@ var (
 		prometheus.HistogramOpts{
 			Name:    "cells_and_proofs_from_structured_computation_milliseconds",
 			Help:    "Captures the time taken to compute cells and proofs from structured computation.",
-			Buckets: []float64{25, 50, 100, 250, 500, 750, 1000},
+			Buckets: []float64{10, 20, 30, 40, 50, 100, 200},
 		},
 	)
 )
