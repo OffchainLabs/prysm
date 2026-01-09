2f4d0bb5d8af843e6ea387bbb1502576f316ec0d
marukai67 <miserly.pendant0p@icloud.com>
2026-01-09 16:56:00 +0100

    Update deprecated_flags.go

--- config/features/deprecated_flags.go
+++ config/features/deprecated_flags.go
@@ -4,18 +4,6 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-// Deprecated flags list.
-const deprecatedUsage = "DEPRECATED. DO NOT USE."
-
-var (
-	// To deprecate a feature flag, first copy the example below, then insert deprecated flag in `deprecatedFlags`.
-	exampleDeprecatedFeatureFlag = &cli.StringFlag{
-		Name:   "name",
-		Usage:  deprecatedUsage,
-		Hidden: true,
-	}
-)
-
 // Deprecated flags for both the beacon node and validator client.
 var deprecatedFlags = []cli.Flag{}
 
