d82b0f4a86a9826d58bfdbf1484bd42ffd118a3a
Bastin <bastin.m@proton.me>
2026-01-26 16:54:23 +0100

    fix bug in check-logs

--- hack/check-logs.sh
+++ hack/check-logs.sh
@@ -8,12 +8,13 @@ cd "$ROOT_DIR"
 ./hack/gen-logs.sh
 
 # Fail if that changed anything
-if ! git diff --quiet -- ./; then
+if ! git diff --quiet -- ./ || [[ -n "$(git ls-files --others --exclude-standard -- ./)" ]]; then
   echo "ERROR: log.go files are out of date. Please run:"
   echo "  ./hack/gen-logs.sh"
   echo "and commit the changes."
   echo
   git diff --stat -- ./ || true
+  git status --porcelain -- ./ || true
   exit 1
 fi
 
