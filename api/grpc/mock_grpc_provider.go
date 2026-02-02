0a582f4970ee93a05e0e749233e31577026bd3eb
james-prysm <james@prysmaticlabs.com>
2026-02-02 15:09:38 -0600

    changing last host comparison to connection counter to avoid race conditions using the same host but an old connection

--- api/grpc/mock_grpc_provider.go
+++ api/grpc/mock_grpc_provider.go
@@ -7,6 +7,7 @@ type MockGrpcProvider struct {
 	MockConn     *grpc.ClientConn
 	MockHosts    []string
 	CurrentIndex int
+	ConnCounter  uint64
 }
 
 func (m *MockGrpcProvider) CurrentConn() *grpc.ClientConn { return m.MockConn }
@@ -16,6 +17,11 @@ func (m *MockGrpcProvider) CurrentHost() string {
 	}
 	return ""
 }
-func (m *MockGrpcProvider) Hosts() []string          { return m.MockHosts }
-func (m *MockGrpcProvider) SwitchHost(idx int) error { m.CurrentIndex = idx; return nil }
-func (m *MockGrpcProvider) Close()                   {}
+func (m *MockGrpcProvider) Hosts() []string { return m.MockHosts }
+func (m *MockGrpcProvider) SwitchHost(idx int) error {
+	m.CurrentIndex = idx
+	m.ConnCounter++
+	return nil
+}
+func (m *MockGrpcProvider) ConnectionCounter() uint64 { return m.ConnCounter }
+func (m *MockGrpcProvider) Close()                    {}
