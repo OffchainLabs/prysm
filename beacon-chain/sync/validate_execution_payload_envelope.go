c7885e694ea3e0c902f6b506012c3c555eba9ffa
terence <terence@prysmaticlabs.com>
2026-02-27 08:14:30 -0800

    Set validator data for payload envelope for gossip

--- beacon-chain/sync/validate_execution_payload_envelope.go
+++ beacon-chain/sync/validate_execution_payload_envelope.go
@@ -126,6 +126,7 @@ func (s *Service) validateExecutionPayloadEnvelope(ctx context.Context, pid peer
 		return pubsub.ValidationReject, err
 	}
 	s.setSeenPayloadEnvelope(root, env.BuilderIndex())
+	msg.ValidatorData = signedEnvelope
 	return pubsub.ValidationAccept, nil
 }
 
