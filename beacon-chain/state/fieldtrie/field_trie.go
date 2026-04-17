21cb002737dbea97622fc831764c0bde4c6fa92c
Manu NALEPA <enalepa@offchainlabs.com>
2026-04-17 21:45:54 +0200

    Update beacon-chain/state/fieldtrie/field_trie.go
    Co-authored-by: Bastin <43618253+Inspector-Butters@users.noreply.github.com>

--- beacon-chain/state/fieldtrie/field_trie.go
+++ beacon-chain/state/fieldtrie/field_trie.go
@@ -177,7 +177,7 @@ func (f *FieldTrie) TrieRoot() ([32]byte, error) {
 // is returned.
 func (f *FieldTrie) RecomputeTrie(indices []uint64, elements any) (*FieldTrie, [32]byte, error) {
 	if indices != nil {
-		// Deduplicateing indices to avoid redundant recomputation.
+		// Deduplicating indices to avoid redundant recomputation.
 		indices = slice.SetUint64(indices)
 	}
 
