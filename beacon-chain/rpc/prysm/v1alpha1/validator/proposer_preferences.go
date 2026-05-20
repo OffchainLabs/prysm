8da90686d97ba29cd11233b2defa08af120a9015
james-prysm <james@prysmaticlabs.com>
2026-05-20 14:51:19 -0500

    gofmt

--- beacon-chain/rpc/prysm/v1alpha1/validator/proposer_preferences.go
+++ beacon-chain/rpc/prysm/v1alpha1/validator/proposer_preferences.go
@@ -85,7 +85,7 @@ func (vs *Server) SubmitSignedProposerPreferences(
 			DependentRoot:  bytesutil.ToBytes32(msg.Message.DependentRoot),
 			ValidatorIndex: valIdx,
 			FeeRecipient:   bytesutil.ToBytes20(msg.Message.FeeRecipient),
-			TargetGasLimit:       msg.Message.TargetGasLimit,
+			TargetGasLimit: msg.Message.TargetGasLimit,
 		}, proposalSlot)
 		if !added {
 			duplicate++
