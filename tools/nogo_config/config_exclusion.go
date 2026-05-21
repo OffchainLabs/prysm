fbce5c59397147cd377662e283c6f91028da0998
Preston Van Loon <preston@pvl.dev>
2026-05-21 11:05:49 -0500

    nogo(nilaway): fix nil config exclusion map initialization

--- tools/nogo_config/config_exclusion.go
+++ tools/nogo_config/config_exclusion.go
@@ -2,16 +2,17 @@ package main
 
 // AddExclusion adds an exclusion to the Configs, if they do not exist already.
 func (c Configs) AddExclusion(check string, exclusions []string) {
+	if c == nil {
+		return
+	}
 	for _, e := range exclusions {
-		if cc, ok := c[check]; !ok {
-			c[check] = Config{
-				ExcludeFiles: make(map[string]string),
-			}
-		} else if cc.ExcludeFiles == nil {
-			cc.ExcludeFiles = make(map[string]string)
+		cfg := c[check]
+		if cfg.ExcludeFiles == nil {
+			cfg.ExcludeFiles = make(map[string]string)
 		}
-		if _, ok := c[check].ExcludeFiles[e]; !ok {
-			c[check].ExcludeFiles[e] = exclusionMessage
+		if _, ok := cfg.ExcludeFiles[e]; !ok {
+			cfg.ExcludeFiles[e] = exclusionMessage
 		}
+		c[check] = cfg
 	}
 }
