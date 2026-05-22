dbb6f57d2113fb12f7d471ca5acb49951cc8992d
Preston Van Loon <preston@pvl.dev>
2026-05-21 19:27:22 -0500

    nogo(nilaway): guard attestation cache lookups

--- beacon-chain/operations/attestations/kv/aggregated.go
+++ beacon-chain/operations/attestations/kv/aggregated.go
@@ -204,7 +204,14 @@ func (c *AttCaches) AggregatedAttestationsBySlotIndex(
 	c.aggregatedAttLock.RLock()
 	defer c.aggregatedAttLock.RUnlock()
 	for _, as := range c.aggregatedAtt {
-		if as[0].Version() == version.Phase0 && slot == as[0].GetData().Slot && committeeIndex == as[0].GetData().CommitteeIndex {
+		if len(as) == 0 {
+			continue
+		}
+		data := as[0].GetData()
+		if data == nil {
+			continue
+		}
+		if as[0].Version() == version.Phase0 && slot == data.Slot && committeeIndex == data.CommitteeIndex {
 			for _, a := range as {
 				att, ok := a.(*ethpb.Attestation)
 				// This will never fail in practice because we asserted the version
@@ -233,7 +240,14 @@ func (c *AttCaches) AggregatedAttestationsBySlotIndexElectra(
 	c.aggregatedAttLock.RLock()
 	defer c.aggregatedAttLock.RUnlock()
 	for _, as := range c.aggregatedAtt {
-		if as[0].Version() >= version.Electra && slot == as[0].GetData().Slot && as[0].CommitteeBitsVal().BitAt(uint64(committeeIndex)) {
+		if len(as) == 0 {
+			continue
+		}
+		data := as[0].GetData()
+		if data == nil {
+			continue
+		}
+		if as[0].Version() >= version.Electra && slot == data.Slot && as[0].CommitteeBitsVal().BitAt(uint64(committeeIndex)) {
 			for _, a := range as {
 				att, ok := a.(*ethpb.AttestationElectra)
 				// This will never fail in practice because we asserted the version
