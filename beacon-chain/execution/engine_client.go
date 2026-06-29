bc9873e50b684a4fb089dbf75e3c34df1fdb9b95
Jun Song <syjn99@gmail.com>
2026-06-29 15:22:58 +0900

    Move execution block fetcher code into execution_block.go

--- beacon-chain/execution/engine_client.go
+++ beacon-chain/execution/engine_client.go
@@ -3,13 +3,11 @@ package execution
 import (
 	"context"
 	"fmt"
-	"math/big"
 	"time"
 
 	"github.com/OffchainLabs/go-bitfield"
 	"github.com/OffchainLabs/prysm/v7/api/server/structs"
 	"github.com/OffchainLabs/prysm/v7/beacon-chain/core/peerdas"
-	"github.com/OffchainLabs/prysm/v7/beacon-chain/execution/types"
 	"github.com/OffchainLabs/prysm/v7/beacon-chain/verification"
 	"github.com/OffchainLabs/prysm/v7/cmd/beacon-chain/flags"
 	fieldparams "github.com/OffchainLabs/prysm/v7/config/fieldparams"
@@ -24,11 +22,7 @@ import (
 	ethpb "github.com/OffchainLabs/prysm/v7/proto/prysm/v1alpha1"
 	"github.com/OffchainLabs/prysm/v7/runtime/version"
 	"github.com/OffchainLabs/prysm/v7/time/slots"
-	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	gethRPC "github.com/ethereum/go-ethereum/rpc"
-	"github.com/holiman/uint256"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sync/errgroup"
@@ -418,170 +412,6 @@ func (s *Service) ExchangeCapabilities(ctx context.Context) ([]string, error) {
 	}
 
 	return elSupportedEndpointsSlice, nil
-}
-
-// GetTerminalBlockHash returns the valid terminal block hash based on total difficulty.
-//
-// Spec code:
-// def get_pow_block_at_terminal_total_difficulty(pow_chain: Dict[Hash32, PowBlock]) -> Optional[PowBlock]:
-//
-//	# `pow_chain` abstractly represents all blocks in the PoW chain
-//	for block in pow_chain:
-//	    parent = pow_chain[block.parent_hash]
-//	    block_reached_ttd = block.total_difficulty >= TERMINAL_TOTAL_DIFFICULTY
-//	    parent_reached_ttd = parent.total_difficulty >= TERMINAL_TOTAL_DIFFICULTY
-//	    if block_reached_ttd and not parent_reached_ttd:
-//	        return block
-//
-//	return None
-func (s *Service) GetTerminalBlockHash(ctx context.Context, transitionTime uint64) ([]byte, bool, error) {
-	ttd := new(big.Int)
-	ttd.SetString(params.BeaconConfig().TerminalTotalDifficulty, 10)
-	terminalTotalDifficulty, overflows := uint256.FromBig(ttd)
-	if overflows {
-		return nil, false, errors.New("could not convert terminal total difficulty to uint256")
-	}
-	blk, err := s.LatestExecutionBlock(ctx)
-	if err != nil {
-		return nil, false, errors.Wrap(err, "could not get latest execution block")
-	}
-	if blk == nil {
-		return nil, false, errors.New("latest execution block is nil")
-	}
-
-	for {
-		if ctx.Err() != nil {
-			return nil, false, ctx.Err()
-		}
-		currentTotalDifficulty, err := tDStringToUint256(blk.TotalDifficulty)
-		if err != nil {
-			return nil, false, errors.Wrap(err, "could not convert total difficulty to uint256")
-		}
-		blockReachedTTD := currentTotalDifficulty.Cmp(terminalTotalDifficulty) >= 0
-
-		parentHash := blk.ParentHash
-		if parentHash == params.BeaconConfig().ZeroHash {
-			return nil, false, nil
-		}
-		parentBlk, err := s.ExecutionBlockByHash(ctx, parentHash, false /* no txs */)
-		if err != nil {
-			return nil, false, errors.Wrap(err, "could not get parent execution block")
-		}
-		if parentBlk == nil {
-			return nil, false, errors.New("parent execution block is nil")
-		}
-
-		if blockReachedTTD {
-			parentTotalDifficulty, err := tDStringToUint256(parentBlk.TotalDifficulty)
-			if err != nil {
-				return nil, false, errors.Wrap(err, "could not convert total difficulty to uint256")
-			}
-
-			// If terminal block has time same timestamp or greater than transition time,
-			// then the node violates the invariant that a block's timestamp must be
-			// greater than its parent's timestamp. Execution layer will reject
-			// a fcu call with such payload attributes. It's best that we return `None` in this a case.
-			parentReachedTTD := parentTotalDifficulty.Cmp(terminalTotalDifficulty) >= 0
-			if !parentReachedTTD {
-				if blk.Time >= transitionTime {
-					return nil, false, nil
-				}
-
-				log.WithFields(logrus.Fields{
-					"number":   blk.Number,
-					"hash":     fmt.Sprintf("%#x", bytesutil.Trunc(blk.Hash[:])),
-					"td":       blk.TotalDifficulty,
-					"parentTd": parentBlk.TotalDifficulty,
-					"ttd":      terminalTotalDifficulty,
-				}).Info("Retrieved terminal block hash")
-				return blk.Hash[:], true, nil
-			}
-		} else {
-			return nil, false, nil
-		}
-		blk = parentBlk
-	}
-}
-
-// LatestExecutionBlock fetches the latest execution engine block by calling
-// eth_blockByNumber via JSON-RPC.
-func (s *Service) LatestExecutionBlock(ctx context.Context) (*pb.ExecutionBlock, error) {
-	ctx, span := trace.StartSpan(ctx, "powchain.engine-api-client.LatestExecutionBlock")
-	defer span.End()
-
-	result := &pb.ExecutionBlock{}
-	err := s.rpcClient.CallContext(
-		ctx,
-		result,
-		BlockByNumberMethod,
-		"latest",
-		false, /* no full transaction objects */
-	)
-	return result, handleRPCError(err)
-}
-
-// ExecutionBlockByHash fetches an execution engine block by hash by calling
-// eth_blockByHash via JSON-RPC.
-func (s *Service) ExecutionBlockByHash(ctx context.Context, hash common.Hash, withTxs bool) (*pb.ExecutionBlock, error) {
-	ctx, span := trace.StartSpan(ctx, "powchain.engine-api-client.ExecutionBlockByHash")
-	defer span.End()
-	result := &pb.ExecutionBlock{}
-	err := s.rpcClient.CallContext(ctx, result, BlockByHashMethod, hash, withTxs)
-	return result, handleRPCError(err)
-}
-
-// ExecutionBlocksByHashes fetches a batch of execution engine blocks by hash by calling
-// eth_blockByHash via JSON-RPC.
-func (s *Service) ExecutionBlocksByHashes(ctx context.Context, hashes []common.Hash, withTxs bool) ([]*pb.ExecutionBlock, error) {
-	_, span := trace.StartSpan(ctx, "powchain.engine-api-client.ExecutionBlocksByHashes")
-	defer span.End()
-	numOfHashes := len(hashes)
-	elems := make([]gethRPC.BatchElem, 0, numOfHashes)
-	execBlks := make([]*pb.ExecutionBlock, 0, numOfHashes)
-	if numOfHashes == 0 {
-		return execBlks, nil
-	}
-	for _, h := range hashes {
-		blk := &pb.ExecutionBlock{}
-		newH := h
-		elems = append(elems, gethRPC.BatchElem{
-			Method: BlockByHashMethod,
-			Args:   []any{newH, withTxs},
-			Result: blk,
-			Error:  error(nil),
-		})
-		execBlks = append(execBlks, blk)
-	}
-	ioErr := s.rpcClient.BatchCall(elems)
-	if ioErr != nil {
-		return nil, ioErr
-	}
-	for _, e := range elems {
-		if e.Error != nil {
-			return nil, handleRPCError(e.Error)
-		}
-	}
-	return execBlks, nil
-}
-
-// HeaderByHash returns the relevant header details for the provided block hash.
-func (s *Service) HeaderByHash(ctx context.Context, hash common.Hash) (*types.HeaderInfo, error) {
-	var hdr *types.HeaderInfo
-	err := s.rpcClient.CallContext(ctx, &hdr, BlockByHashMethod, hash, false /* no transactions */)
-	if err == nil && hdr == nil {
-		err = ethereum.NotFound
-	}
-	return hdr, err
-}
-
-// HeaderByNumber returns the relevant header details for the provided block number.
-func (s *Service) HeaderByNumber(ctx context.Context, number *big.Int) (*types.HeaderInfo, error) {
-	var hdr *types.HeaderInfo
-	err := s.rpcClient.CallContext(ctx, &hdr, BlockByNumberMethod, toBlockNumArg(number), false /* no transactions */)
-	if err == nil && hdr == nil {
-		err = ethereum.NotFound
-	}
-	return hdr, err
 }
 
 // GetBlobs returns the blob and proof from the execution engine for the given versioned hashes.
@@ -1263,18 +1093,6 @@ func fullPayloadFromPayloadBody(
 	return nil, fmt.Errorf("unknown execution block version for payload %s", version.String(bVersion))
 }
 
-func tDStringToUint256(td string) (*uint256.Int, error) {
-	b, err := hexutil.DecodeBig(td)
-	if err != nil {
-		return nil, err
-	}
-	i, overflows := uint256.FromBig(b)
-	if overflows {
-		return nil, errors.New("total difficulty overflowed")
-	}
-	return i, nil
-}
-
 func EmptyExecutionPayload(v int) (proto.Message, error) {
 	if v >= version.Gloas {
 		return &pb.ExecutionPayloadGloas{
@@ -1391,25 +1209,6 @@ func EmptyExecutionPayloadHeader(v int) (proto.Message, error) {
 	}
 
 	return nil, errors.Wrapf(ErrUnsupportedVersion, "version=%s", version.String(v))
-}
-
-func toBlockNumArg(number *big.Int) string {
-	if number == nil {
-		return "latest"
-	}
-	pending := big.NewInt(-1)
-	if number.Cmp(pending) == 0 {
-		return "pending"
-	}
-	finalized := big.NewInt(int64(gethRPC.FinalizedBlockNumber))
-	if number.Cmp(finalized) == 0 {
-		return "finalized"
-	}
-	safe := big.NewInt(int64(gethRPC.SafeBlockNumber))
-	if number.Cmp(safe) == 0 {
-		return "safe"
-	}
-	return hexutil.EncodeBig(number)
 }
 
 // wrapWithBlockRoot returns a new error with the given block root.
