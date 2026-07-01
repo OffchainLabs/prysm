f0e00c988671580c042ed57000773d665c3eb109
terence <terence@prysmaticlabs.com>
2026-07-01 13:50:48 -0700

    James feedback and fix tests

--- api/client/builder/client_gloas.go
+++ api/client/builder/client_gloas.go
@@ -153,11 +153,7 @@ func (c *Client) SubmitSignedBeaconBlock(ctx context.Context, sb interfaces.Read
 	if err != nil {
 		return errors.Wrap(err, "could not json encode SignedBeaconBlock")
 	}
-	jsonOpts := func(r *http.Request) {
-		r.Header.Set("Content-Type", api.JsonMediaType)
-		r.Header.Set(api.VersionHeader, version.String(sb.Version()))
-	}
-	if _, _, err := c.do(ctx, http.MethodPost, postBeaconBlockPath, bytes.NewReader(jsonBody), http.StatusAccepted, jsonOpts); err != nil {
+	if _, _, err := c.do(ctx, http.MethodPost, postBeaconBlockPath, bytes.NewReader(jsonBody), http.StatusAccepted, contentTypeOpts(api.JsonMediaType, sb.Version())); err != nil {
 		return errors.Wrap(err, "error submitting json signed beacon block to builder")
 	}
 	return nil
