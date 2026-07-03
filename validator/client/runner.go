92b441fb697d4a666966c0329709ce4aaa4f0bab
Jun Song <syjn99@gmail.com>
2026-07-03 15:04:04 +0900

    Restore the comment

--- validator/client/runner.go
+++ validator/client/runner.go
@@ -273,6 +273,8 @@ func performRoles(slotCtx context.Context, allRoles map[[48]byte][]iface.Validat
 		}()
 		// Log submissions from the current slot
 		v.LogSubmissions(slot)
+
+		// Log performance in the previous slot
 		if err := v.LogValidatorGainsAndLosses(slotCtx, slot); err != nil {
 			log.WithError(err).Error("Could not report validator's rewards/penalties")
 		}
