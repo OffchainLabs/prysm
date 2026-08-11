7c4cc9ab9ca57fc9954c2c1bad0d7f070600b3a3
Aarsh Shah <aarshkshah1992@gmail.com>
2026-08-11 16:48:55 +0400

    fix container ordering

--- testing/spectest/shared/gloas/ssz_static/ssz_static.go
+++ testing/spectest/shared/gloas/ssz_static/ssz_static.go
@@ -75,6 +75,8 @@ func unmarshalledSSZ(t *testing.T, serializedBytes []byte, folderName string) (a
 		obj = &ethpb.DataColumnSidecarGloas{}
 	case "SignedProposerPreferences", "ProposerPreferences":
 		t.Skip("p2p-only type; not part of the consensus state transition")
+	case "PartialDataColumnGroupID":
+		obj = &ethpb.PartialDataColumnGroupID{}
 
 	// Standard types that also exist in gloas
 	case "ExecutionPayload":
