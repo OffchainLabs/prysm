6235511cd8ed0c80d2d4b50db3755a2750a67362
terence <terence@prysmaticlabs.com>
2026-08-19 13:31:00 -0700

    Return on unsupported block version instead of falling through with a nil block

--- validator/client/propose.go
+++ validator/client/propose.go
@@ -168,6 +168,10 @@ func (v *validator) ProposeBlock(ctx context.Context, slot primitives.Slot, pubK
 			}
 		default:
 			log.Errorf("Unsupported block version %s", version.String(blk.Version()))
+			if v.emitAccountMetrics {
+				ValidatorProposeFailVec.WithLabelValues(fmtKey).Inc()
+			}
+			return
 		}
 	} else {
 		genericSignedBlock, err = blk.PbGenericBlock()
