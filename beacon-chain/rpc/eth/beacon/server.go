9cde43d333bc37ec66aa1b72907aa4e0e542774e
terence <terence@prysmaticlabs.com>
2026-09-18 20:28:47 -0700

    Broadcast published envelopes without block checks at gossip validation level

--- beacon-chain/rpc/eth/beacon/server.go
+++ beacon-chain/rpc/eth/beacon/server.go
@@ -20,7 +20,6 @@ import (
 	"github.com/OffchainLabs/prysm/v7/beacon-chain/rpc/lookup"
 	"github.com/OffchainLabs/prysm/v7/beacon-chain/state/stategen"
 	"github.com/OffchainLabs/prysm/v7/beacon-chain/sync"
-	"github.com/OffchainLabs/prysm/v7/beacon-chain/verification"
 	eth "github.com/OffchainLabs/prysm/v7/proto/prysm/v1alpha1"
 )
 
@@ -56,6 +55,4 @@ type Server struct {
 	ForkchoiceFetcher          blockchain.ForkchoiceFetcher
 	CoreService                *core.Service
 	AttestationStateFetcher    blockchain.AttestationStateFetcher
-	// PayloadEnvelopeVerifier runs gossip-level checks on published envelopes.
-	PayloadEnvelopeVerifier verification.NewExecutionPayloadEnvelopeVerifier
 }
