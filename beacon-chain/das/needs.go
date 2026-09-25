81d778bfedcd852ac00df3f062f6fac49cb6da87
satushh <satushh@gmail.com>
2026-09-25 17:02:29 +0100

    Drop MaxSafeEpoch clamp for the scheduled Fulu fork in NewSyncNeeds

--- beacon-chain/das/needs.go
+++ beacon-chain/das/needs.go
@@ -52,8 +52,7 @@ func NewSyncNeeds(current CurrentSlotter, oldestSlotFlagPtr *primitives.Slot, bl
 	if err != nil {
 		return SyncNeeds{}, errors.Wrap(err, "deneb fork slot")
 	}
-	fuluBoundary := min(params.BeaconConfig().FuluForkEpoch, slots.MaxSafeEpoch())
-	fulu, err := slots.EpochStart(fuluBoundary)
+	fulu, err := slots.EpochStart(params.BeaconConfig().FuluForkEpoch)
 	if err != nil {
 		return SyncNeeds{}, errors.Wrap(err, "fulu fork slot")
 	}
